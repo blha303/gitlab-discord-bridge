fce0a5cf99e1c1c6b0b015b94681deb66a61943a
Steven Smith <stevensmith.ome@gmail.com>
2016-10-16 15:15:14 +0800

    Working

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-_globals = globals
+#!/usr/bin/env python3
 from flask import *
+from requests import post
+from json import load, dump
+
+with open("config.json") as f:
+    config = json.load(f)
 
 app = Flask(__name__)
 
@@ -7,13 +12,12 @@ def handle_push(body):
     """ Handle GitLab push event webhook
         https://gitlab.com/gitlab-org/gitlab-ce/blob/master/doc/web_hooks/web_hooks.md#push-events """
     tcc = body["total_commits_count"]
-    outp = { "number": tcc if tcc > 1 else "a",
-             "cmt_plural": "s" if tcc != 1 else "",
-             "commits": "\n".join("{id:.5} **{author[name]}**: {message}".format(**c) for c in body["commits"])
-           }
+    body.update({"number": tcc if tcc > 1 else "a",
+            "cmt_plural": "s" if tcc != 1 else "",
+            "commits": "\n".join("`{id:.7}` **{author[name]}**: {message}".format(**c) for c in body["commits"]) })
     return """{project[web_url]}/compare/{before}...{after}
 {user_name} pushed {number} commit{cmt_plural}:
-{commits}""".format(**outp)
+{commits}""".format(**body)
 
 def handle_tag(body):
     """ Handle GitLab tag push event webhook
@@ -54,13 +58,20 @@ def handle_pipeline(body):
 
 def post_to_discord(channel, text):
     """ Posts to Discord like a boss """
-    pass
+    print(text)
+    d = post("https://discordapp.com/api/channels/{}/messages".format(channel),
+             json={"content": text},
+             headers={"Authorization": config["token"],
+                      "User-Agent": "gitlab-discord-bridge by blha303"
+                     }).json()
+    print(d)
+    return "cool"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method != "POST":
         return make_response("lol", 400)
-    if request.remote_addr != "103.245.212.8":
+    if request.remote_addr != "103.245.212.8" or request.headers.get("X-Gitlab-Token", "") != config["secret"]:
         return make_response("go away please", 403)
     try:
         body = request.get_json()
@@ -76,5 +87,8 @@ def index():
                 "pipeline": handle_pipeline
                }
     if body["object_kind"] in handlers:
-        post_to_discord( handlers[body["object_kind"]](body) )
-    return make_response("", 200)
+        return make_response(post_to_discord(config["channel"], handlers[body["object_kind"]](body) ), 200)
+    return make_response("wat", 400)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=25431, host="0.0.0.0")
