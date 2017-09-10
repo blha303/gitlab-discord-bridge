84443afb985d02f1225e92661a5cb502ae8672ac
Milan Stastny <milan@stastnej.ch>
2017-09-10 13:39:40 +0200

    Reformat the file

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ with open("config.json") as f:
 
 app = Flask(__name__)
 
+
 def handle_push(body):
     """ Handle GitLab push event webhook
         https://gitlab.com/gitlab-org/gitlab-ce/blob/master/doc/web_hooks/web_hooks.md#push-events """
@@ -18,10 +19,11 @@ def handle_push(body):
                  "number": tcc if tcc > 1 else "a",
                  "cmt_plural": "s" if tcc != 1 else "",
                  "commits": "\n".join(commitfmt.format(**c) for c in body["commits"])
-                })
+                 })
     return """:round_pushpin: <{project[web_url]}/commits/{branch}>
 **{user_name}** pushed {number} commit{cmt_plural}:
 {commits}""".format(**body)
+
 
 def handle_tag(body):
     """ Handle GitLab tag push event webhook
@@ -29,6 +31,7 @@ def handle_tag(body):
     body.update({"tag": body["ref"].split("/")[-1]})
     return """:label: <{project[web_url]}/tree/{tag}>
 **{user_name}** pushed a tag: {tag}""".format(**body)
+
 
 def handle_issue(body):
     """ Handle GitLab issue event webhook
@@ -43,33 +46,40 @@ def handle_issue(body):
     return """:page_facing_up: <{project[web_url]}/issues/{object_attributes[id]}>
 **{user[name]}** {action} an issue: {object_attributes[title]}""".format(**body)
 
+
 def handle_note(body):
     """ Handle GitLab comment (note) event webhook
     https://gitlab.com/gitlab-org/gitlab-ce/blob/master/doc/web_hooks/web_hooks.md#comment-events """
+
     def convert(type):
         s1 = sub('(.)([A-Z][a-z]+)', r'\1 \2', type)
         return sub('([a-z0-9])([A-Z])', r'\1 \2', s1).lower()
-    body.update({"type": convert(body["object_attributes"]["noteable_type"]) })
+
+    body.update({"type": convert(body["object_attributes"]["noteable_type"])})
     return """:notepad_spiral: <{object_attributes[url]}>
 **{user[name]}** commented on a {type}:
 {object_attributes[note]}""".format(**body)
+
 
 def handle_merge(body):
     """ Handle GitLab merge request event webhook
         https://gitlab.com/gitlab-org/gitlab-ce/blob/master/doc/user/project/integrations/webhooks.md#push-events """
     try:
-        url=body["object_attributes"]["url"]
+        url = body["object_attributes"]["url"]
     except KeyError:
-        url=body["project"]["web_url"] + "/merge_requests"
-    body["object_attributes"]["url"]=url
+        url = body["project"]["web_url"] + "/merge_requests"
+    body["object_attributes"]["url"] = url
     return """:arrows_counterclockwise: <{object_attributes[url]}>
-**{user[name]}** created a merge request: {object_attributes[source_branch]}->{object_attributes[target_branch]} **{object_attributes[title]}**""".format(**body)
+**{user[name]}** created a merge request: {object_attributes[source_branch]}->{object_attributes[target_branch]} **{object_attributes[title]}**""".format(
+        **body)
+
 
 def handle_wiki(body):
     """ Handle GitLab wiki page event webhook
         https://gitlab.com/gitlab-org/gitlab-ce/blob/master/doc/web_hooks/web_hooks.md#wiki-page-events """
     return """:notebook: <{object_attributes[url]}>
 **{user[name]}** created a wiki page: {object_attributes[title]}""".format(**body)
+
 
 def handle_pipeline(body):
     """ Handle GitLab pipeline event webhook
@@ -80,12 +90,15 @@ def handle_pipeline(body):
                 **{object_attributes[ref]}**: Pipeline created by **{user[name]}**""".format(**body)
     if body["object_attributes"]["status"] == "success":
         return """:bathtub: {project[web_url]}/pipelines/{object_attributes[id]}
-                **{object_attributes[ref]}**: Pipeline finished after **{object_attributes[duration]:.0f}** seconds""".format(**body)
+                **{object_attributes[ref]}**: Pipeline finished after **{object_attributes[duration]:.0f}** seconds""".format(
+            **body)
     if body["object_attributes"]["status"] == "running":
         return """:bathtub: {project[web_url]}/pipelines/{object_attributes[id]}
                 **{object_attributes[ref]}**: Pipeline started""".format(**body)
     return """:bathtub: {project[web_url]}/pipelines/{object_attributes[id]}
-            **{object_attributes[ref]}**: Pipeline {object_attributes[status]} after **{object_attributes[duration]:.0f}** seconds""".format(**body)
+            **{object_attributes[ref]}**: Pipeline {object_attributes[status]} after **{object_attributes[duration]:.0f}** seconds""".format(
+        **body)
+
 
 def handle_job(body):
     """ Handle GitLab pipeline event webhook
@@ -113,14 +126,16 @@ def post_to_discord(channel, text):
              json={"content": text},
              headers={"Authorization": "Bot " + config["token"],
                       "User-Agent": "gitlab-discord-bridge by blha303"
-                     }).json()
+                      }).json()
     return "cool"
+
 
 @app.route('/<channelid>', methods=['GET', 'POST'])
 def index(channelid):
     if request.method != "POST":
         return make_response("lol", 400)
-    if ("host" in config and request.remote_addr != config["host"]) or request.headers.get("X-Gitlab-Token", "") != config["secret"]:
+    if ("host" in config and request.remote_addr != config["host"]) or request.headers.get("X-Gitlab-Token", "") != \
+            config["secret"]:
         return make_response("go away please", 403)
     try:
         body = request.get_json()
@@ -135,11 +150,12 @@ def index(channelid):
                 "wiki_page": handle_wiki,
                 "pipeline": handle_pipeline,
                 "build": handle_job
-               }
+                }
     if body["object_kind"] in handlers:
-        return make_response(post_to_discord(channelid, handlers[body["object_kind"]](body) ), 200)
+        return make_response(post_to_discord(channelid, handlers[body["object_kind"]](body)), 200)
     print(body)
     return make_response("wat", 400)
+
 
 if __name__ == "__main__":
     app.run(debug=False, port=25431, host="0.0.0.0")
